06465a8ec21e5efe5336129809ddb756982103aa
가은 <rkdms8228@naver.com>
2022-04-24 22:45:13 +0900

    song

---
+++ src/com/javaex/ex03/SongApp.java
@@ -0,0 +1,12 @@
+package com.javaex.ex03;
+
+public class SongApp {
+
+	public static void main(String[] args) {
+		
+		Song s1 = new Song("좋은날", "아이유", "Real", "이민수", "2010", 3);
+		//생성자가 인스턴스화를 하며 출력하는 파라미너 5개와 파라미터 6개만 출력되고 아무것도 나타나지 않을 것을 예상
+		
+	}
+
+}

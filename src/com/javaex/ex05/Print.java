d40e1e91031962a0c6690fd081baf0af15b6c775
가은 <rkdms8228@naver.com>
2022-04-24 23:34:59 +0900

    print

---
+++ src/com/javaex/ex05/Print.java
@@ -0,0 +1,42 @@
+package com.javaex.ex05;
+
+public class Print {
+
+	//필드
+	private int num;
+	private String fact;
+	private double lowNum;
+	private String name;
+	
+	//생성자
+	
+	
+	//메소드-gs
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getFact() {
+		return fact;
+	}
+	public void setFact(String fact) {
+		this.fact = fact;
+	}
+	public double getLowNum() {
+		return lowNum;
+	}
+	public void setLowNum(double lowNum) {
+		this.lowNum = lowNum;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	//메소드-일반
+	
+}
---
+++ src/com/javaex/ex05/PrintApp.java
@@ -0,0 +1,26 @@
+package com.javaex.ex05;
+
+public class PrintApp {
+
+	public static void main(String[] args) {
+
+		Print point = new Print();
+		point.setNum(10);
+		
+		Print result = new Print();
+		result.setFact("true");
+		
+		Print lowPoint = new Print();
+		lowPoint.setLowNum(5.7);
+		
+		Print name = new Print();
+		name.setName("홍길동");
+		
+		System.out.println(point.getNum());
+		System.out.println(result.getFact());
+		System.out.println(lowPoint.getLowNum());
+		System.out.println(name.getName());
+
+	}
+
+}

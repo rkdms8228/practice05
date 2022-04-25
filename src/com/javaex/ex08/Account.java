06649eb7a4a5531164790c76ac7e77e3425e020c
가은 <rkdms8228@naver.com>
2022-04-25 18:05:38 +0900

    account

---
+++ src/com/javaex/ex08/Account.java
@@ -0,0 +1,33 @@
+package com.javaex.ex08;
+
+public class Account {
+
+	private String accountNo;
+	private int balance;
+	private int sum = 0;
+
+	//생성자 작성
+	public Account() {
+	}
+	
+	public Account(String accountNo) {
+		this.accountNo = accountNo;
+
+	}
+	
+	//필요한 메소드 작성
+	//일반
+	public void deposit(int money) {
+		sum = money;
+	}
+	
+
+	public void withdraw(int money) {
+		balance = sum-money;
+	}
+	
+	public void showBalance() {
+		System.out.println(balance);
+	}
+
+}

166a60a6648813a509e6867c054200519d91d6f5
가은 <rkdms8228@naver.com>
2022-05-07 21:38:40 +0900

    1st

---
+++ src/com/javaex/ex10/Book.java
@@ -0,0 +1,5 @@
+package com.javaex.ex10;
+
+public class Book {
+
+}
---
+++ src/com/javaex/ex10/BookApp.java
@@ -0,0 +1,11 @@
+package com.javaex.ex10;
+
+public class BookApp {
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
